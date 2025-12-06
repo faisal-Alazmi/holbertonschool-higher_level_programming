eb56ea9ac06281ca8ae4da548329da886b0cba24
root <root@ip-10-42-95-225.ec2.internal>
2025-12-06 00:53:43 +0000

    fix: 0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Module for add_integer function.
-Provides a function that adds two integers or floats.
+This module provides a function that adds two integers.
 """
 
 
@@ -9,20 +9,15 @@ def add_integer(a, b=98):
     """
     Add two integers or floats and return the result as an integer.
 
-    Float arguments are cast to integers before addition.
-
-    Args:
-        a (int or float): The first number.
-        b (int or float): The second number (default = 98).
+    a and b must be integers or floats. If not, a TypeError is raised.
 
     Returns:
-        int: The integer sum of a and b.
-
-    Raises:
-        TypeError: If a or b is not an integer or float.
+        int: The integer addition of a and b.
     """
-    if not isinstance(a, (int, float)):
+    if not isinstance(a, (int, float)) or a != a:
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+
+    if not isinstance(b, (int, float)) or b != b:
         raise TypeError("b must be an integer")
+
     return int(a) + int(b)
--- python-test_driven_development/README.md
+++ python-test_driven_development/README.md
@@ -1,11 +1,1 @@
-# Python - Test Driven Development
-
-This project focuses on learning and applying Test Driven Development (TDD) in Python.
-You will write documentation and tests before implementing the actual code.
-
-The goal is to understand how to write effective doctest files, how to document modules
-and functions correctly, how to think about edge cases, and how to use doctest to 
-validate the behavior of your program.
-
-This project reinforces clean coding habits, writing reliable code, and ensuring 
-functions behave as documented through automated testing.
+# Python - Test Driven Development`
