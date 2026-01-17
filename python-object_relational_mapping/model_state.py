9c01c3e8d5cddcd8cc0fa08e1498c18908910179
root <root@ip-10-42-177-171.ec2.internal>
2026-01-17 14:27:08 +0000

    model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """
-Contains the class definition of a City.
+Contains the class definition of a State.
 """
 
-from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
-class City(Base):
-    """City class mapped to 'cities' table"""
+class State(Base):
+    """State class mapped to states table"""
 
-    __tablename__ = "cities"
+    __tablename__ = "states"
+
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
