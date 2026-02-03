efd01ea715508aa33648296db5442adaf84e8437
faisal-alazmi <fa41sa@hotmail.com>
2026-02-03 12:56:26 +0300

    Add: 0-script.js

---
+++ javascript-dom_manipulation/0-script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const header = document.querySelector("header");
+  if (!header) {
+    return;
+  }
+
+  header.style.color = "#FF0000";
+});
